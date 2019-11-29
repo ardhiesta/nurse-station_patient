5aa5a40645f980ae1df2399af17ac70ebb7db9a2
hakikid <hakivinamana@student.uns.ac.id>
2019-11-29 08:12:58 +0700

    add buttons and icon

--- app/src/main/java/id/ac/uns/nsp/MainActivity.java
+++ app/src/main/java/id/ac/uns/nsp/MainActivity.java
@@ -1,14 +1,63 @@
 package id.ac.uns.nsp;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-public class MainActivity extends AppCompatActivity {
-
+    private Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(this);
+        btn2 = findViewById(R.id.btn2);
+        btn2.setOnClickListener(this);
+        btn3 = findViewById(R.id.btn3);
+        btn3.setOnClickListener(this);
+        btn4 = findViewById(R.id.btn4);
+        btn4.setOnClickListener(this);
+        btn5 = findViewById(R.id.btn5);
+        btn5.setOnClickListener(this);
+        btn6 = findViewById(R.id.btn6);
+        btn6.setOnClickListener(this);
+        btn7 = findViewById(R.id.btn7);
+        btn7.setOnClickListener(this);
+        btn8 = findViewById(R.id.btn8);
+        btn8.setOnClickListener(this);
+        btn9 = findViewById(R.id.btn9);
+        btn9.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn1:
+
+            case R.id.btn2:
+
+            case R.id.btn3:
+
+            case R.id.btn4:
+
+            case R.id.btn5:
+
+            case R.id.btn6:
+
+            case R.id.btn7:
+
+            case R.id.btn8:
+
+            case R.id.btn9:
+
+        }
     }
 }
